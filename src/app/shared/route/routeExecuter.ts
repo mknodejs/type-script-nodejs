ea32b4f8f2d3818834fdb64f924ae212a53fb204
Mohit Kumar <mkgithub@gmail.com>
2020-03-02 22:33:17 -0500

    V 0.1

--- src/app/shared/route/routeExecuter.ts
+++ src/app/shared/route/routeExecuter.ts
@@ -1,8 +1,8 @@
 import { Request, Response, NextFunction } from 'express'
 import { RouteCallback } from './routeCallback'
-import { IResponse } from '../response/response'
+import { IResponse, ErrorResponse } from '../response/response'
 
-const routeExecuter = (routeCallback: RouteCallback) => {
+const routeExecuter = (routeCallback: RouteCallback, defaultErrorResponse?: ErrorResponse) => {
   const executer = (req: Request, res: Response, next: NextFunction) => {
     const startTime = Date.now()
     try {
@@ -10,6 +10,9 @@ const routeExecuter = (routeCallback: RouteCallback) => {
       res.status(response.httpCode).json(response.data)
     } catch (error) {
       console.log(error)
+      if (defaultErrorResponse) {
+        res.status(defaultErrorResponse.httpCode).json(defaultErrorResponse.data)
+      }
     } finally {
       console.log({ executionTime: Date.now() - startTime })
     }
--- src/app/shared/route/routerBuilder.ts
+++ src/app/shared/route/routerBuilder.ts
@@ -1,23 +1,24 @@
 import { Router } from 'express'
 import { RouteCallback } from './routeCallback'
+import { ErrorResponse } from '../response/response'
 import routeExecuter from './routeExecuter'
 
 export default class RouterBuilder {
-  private router = Router()
-  get = (path: string, cb: RouteCallback) => {
-    this.router.get(path, routeExecuter(cb))
+  private readonly router = Router()
+  get = (path: string, cb: RouteCallback, defaultErrorResponse?: ErrorResponse) => {
+    this.router.get(path, routeExecuter(cb, defaultErrorResponse))
   }
-  post = (path: string, cb: RouteCallback) => {
-    this.router.post(path, routeExecuter(cb))
+  post = (path: string, cb: RouteCallback, defaultErrorResponse?: ErrorResponse) => {
+    this.router.post(path, routeExecuter(cb, defaultErrorResponse))
   }
-  put = (path: string, cb: RouteCallback) => {
-    this.router.put(path, routeExecuter(cb))
+  put = (path: string, cb: RouteCallback, defaultErrorResponse?: ErrorResponse) => {
+    this.router.put(path, routeExecuter(cb, defaultErrorResponse))
   }
-  patch = (path: string, cb: RouteCallback) => {
-    this.router.patch(path, routeExecuter(cb))
+  patch = (path: string, cb: RouteCallback, defaultErrorResponse?: ErrorResponse) => {
+    this.router.patch(path, routeExecuter(cb, defaultErrorResponse))
   }
-  delete = (path: string, cb: RouteCallback) => {
-    this.router.delete(path, routeExecuter(cb))
+  delete = (path: string, cb: RouteCallback, defaultErrorResponse?: ErrorResponse) => {
+    this.router.delete(path, routeExecuter(cb, defaultErrorResponse))
   }
   getRouter = () => {
     return this.router
