306fce967def954a515aabb30b1836b25c96228a
Mohit Kumar <mkgithub@gmail.com>
2020-03-04 18:43:28 -0500

    enrichWithMetaData

--- src/app/shared/logger/index.ts
+++ src/app/shared/logger/index.ts
@@ -7,25 +7,25 @@ interface LogInput {
 
 export const error: LogInput = async (req, data) => {
   if (donotLog(LogLevels.Error)) return
-  logger.log({ level: 'error', message: enrichWithMetadata(req, data) })
+  logger.log({ level: 'error', message: enrichWithMetaData(req, data) })
 }
 
 export const warn: LogInput = async (req, data) => {
   if (donotLog(LogLevels.Warn)) return
-  logger.log({ level: 'warn', message: enrichWithMetadata(req, data) })
+  logger.log({ level: 'warn', message: enrichWithMetaData(req, data) })
 }
 
 export const info: LogInput = async (req, data) => {
   if (donotLog(LogLevels.Info)) return
-  logger.log({ level: 'info', message: enrichWithMetadata(req, data) })
+  logger.log({ level: 'info', message: enrichWithMetaData(req, data) })
 }
 
 export const debug: LogInput = async (req, data) => {
   if (donotLog(LogLevels.Debug)) return
-  logger.log({ level: 'debug', message: enrichWithMetadata(req, data) })
+  logger.log({ level: 'debug', message: enrichWithMetaData(req, data) })
 }
 
-const enrichWithMetadata = (req: Request, data: any): any => {
+const enrichWithMetaData = (req: Request, data: any): any => {
   return {
     ...data,
     nodeId: NODE_ID
