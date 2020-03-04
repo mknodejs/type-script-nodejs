8cce8c93d044859166e092da23731cd1cfb161c4
Mohit Kumar <mkgithub@gmail.com>
2020-03-04 18:40:49 -0500

    node id in logs

--- src/app/shared/logger/config.ts
+++ src/app/shared/logger/config.ts
@@ -6,6 +6,7 @@ export enum LogLevels {
 }
 
 const LOG_LEVEL: LogLevels = LogLevels.Debug
+export const NODE_ID = Date.now() + Math.floor(Math.random() * 1000)
 
 export const donotLog = (logLevels: LogLevels) => {
   if (logLevels < LOG_LEVEL) return true
--- src/app/shared/logger/index.ts
+++ src/app/shared/logger/index.ts
@@ -1,4 +1,4 @@
-import { LogLevels, donotLog } from './config'
+import { LogLevels, donotLog, NODE_ID } from './config'
 import { Request } from 'express'
 import { logger } from './winston'
 interface LogInput {
@@ -7,20 +7,27 @@ interface LogInput {
 
 export const error: LogInput = async (req, data) => {
   if (donotLog(LogLevels.Error)) return
-  logger.log({ level: 'error', message: data })
+  logger.log({ level: 'error', message: enrichWithMetadata(req, data) })
 }
 
 export const warn: LogInput = async (req, data) => {
   if (donotLog(LogLevels.Warn)) return
-  logger.log({ level: 'warn', message: data })
+  logger.log({ level: 'warn', message: enrichWithMetadata(req, data) })
 }
 
 export const info: LogInput = async (req, data) => {
   if (donotLog(LogLevels.Info)) return
-  logger.log({ level: 'info', message: data })
+  logger.log({ level: 'info', message: enrichWithMetadata(req, data) })
 }
 
 export const debug: LogInput = async (req, data) => {
   if (donotLog(LogLevels.Debug)) return
-  logger.log({ level: 'debug', message: data })
+  logger.log({ level: 'debug', message: enrichWithMetadata(req, data) })
+}
+
+const enrichWithMetadata = (req: Request, data: any): any => {
+  return {
+    ...data,
+    nodeId: NODE_ID
+  }
 }
